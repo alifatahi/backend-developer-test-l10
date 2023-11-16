dc15ec7fb7c367a016b853c6414bc3741a5d4ed8
alifatahi <a.fatahi1992@gmail.com>
2023-11-16 19:59:27 +0400

    add sanctum to web router and create a sample api token

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AchievementsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users/{user}/achievements', [AchievementsController::class, 'index']);
+Route::get('/tokens/create', function () {
+    // create a sample token for first user
+    $user = Auth::loginUsingId(1);
+    $token = $user->createToken(request()->token_name);
+    return ['token' => $token->plainTextToken];
+});
+
+//SECTION - users
+Route::group(['prefix'=> 'users', 'middleware'=> ['auth:sanctum']], function () {
+    // user achievements
+    Route::get('{user}/achievements', [AchievementsController::class, 'index']);
+
+});
