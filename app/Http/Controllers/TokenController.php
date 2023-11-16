9d56abadf97ac3f0f18ce7a8126969c4bdb55910
alifatahi <a.fatahi1992@gmail.com>
2023-11-16 21:53:40 +0400

    token/create method

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -7,7 +7,20 @@ use Illuminate\Support\Facades\Auth;
 
 class TokenController extends Controller
 {
-    public function create(Request $request)
+    public function create(Request $request): array|null
     {
+        // attemp to login
+        $credentials = [
+            'email'=> $request->email,
+            'password'=> $request->password,
+        ];
+        if (!Auth::attempt($credentials)) {
+            abort(403);
+        }
+        // get the user after login attempt
+        $user = Auth::user();
+        // create the
+        $token = $user->createToken('test01');
+        return ['token' => $token->plainTextToken];
     }
 }
