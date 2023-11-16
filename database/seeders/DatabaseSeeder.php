4df58a7c242655eadec94e6e4a3382406b8add91
alifatahi <a.fatahi1992@gmail.com>
2023-11-16 22:20:35 +0400

    create achievements table migration and add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Achievement;
 use App\Models\Badge;
 use App\Models\Lesson;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,13 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Lessons
         $lessons = Lesson::factory()
             ->count(20)
             ->create();
 
+        // Badges
         Badge::create(['name' => 'Beginner']);
         Badge::create(['name' => 'Intermediate']);
         Badge::create(['name' => 'Advanced']);
         Badge::create(['name' => 'Master']);
+
+        // Achivements
+        Achievement::create(['name' => 'Lessons Watched']);
+        Achievement::create(['name' => 'Comments Written']);
     }
 }
